f87218bc11e2a5ab2d9e33c464cb53c16d73c6d6
Pavel <kurilo.pavel@mail.ru>
2022-05-15 01:34:32 +0300

    feat: implement slider (Sun, May 15, 2022, 01:34:32)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -1,7 +1,7 @@
 "use strict"
 import {data} from './data.js';
 
-// const body = document.querySelector('body');
+const body = document.querySelector('body');
 
 function createField(tagName, className, parentName) {
   const parent = document.querySelector(parentName)
@@ -12,49 +12,62 @@ function createField(tagName, className, parentName) {
 
 createField('div', 'field', 'body');
 createField('div', 'fieldImg', '.field');
-createField('img', 'img', '.fieldImg');
 createField('div', 'phrase', '.field');
-createField('blockquote', 'text', '.phrase');
 createField('div', 'controls', '.field');
 
+for (let i = 0; i < data.length; i++) {
+  createField('img', 'img', '.fieldImg');
+  createField('blockquote', 'text', '.phrase');
+  createField('span', 'controlsField' + i, '.controls');
+  createField('div', 'circle', '.controlsField' + i);
+}
 
-function addData(number) {
-  const img = document.querySelector('.img');
-  img.setAttribute('src', data[number][1]);
-  const text = document.querySelector('.text');
-  text.innerHTML = data[number][0];
+function addData() {
+  const image = document.querySelectorAll('.img');
+  const text = document.querySelectorAll('.text');
+  const circle = document.querySelectorAll('span');
+  for (let i = 0; i < data.length; i++) {
+    image[i].setAttribute('src', data[i][1]);
+    text[i].innerHTML = data[i][0];
+    circle[i].setAttribute('value', i);
+  }
+}
+
+function start(e) {
+  let image = document.querySelectorAll('img');
+  image.forEach(img => {
+    if (img.className === 'animationStart') {
+      img.className = 'animationEnd'
+    }
+  })
+  image[e.target.getAttribute('value')].setAttribute('class', 'animationStart');
+
 }
 
 addData(0);
-
-
-for (let i = 0; i < data.length; i++) {
-  createField('span', 'controlsField' + i, '.controls');
-  createField('div', 'circle', '.controlsField' + i);
-
-}
-
 const circle = document.querySelectorAll('span');
-circle.forEach(element => element.addEventListener('mouseover', changeBackground))
-circle.forEach(element => element.addEventListener('mouseout', changeBackground))
+circle.forEach(element => element.addEventListener('mouseover', changeBackground));
+circle.forEach(element => element.addEventListener('click', start));
+circle.forEach(element => element.addEventListener('mouseout', changeBackground));
 
 function changeBackground(e) {
   if (e.target.children[0].style.background === 'dodgerblue') {
-    e.target.children[0].style.background = ''
+    e.target.children[0].style.background = '';
   } else {
-    e.target.children[0].style.background = 'dodgerblue'
+    e.target.children[0].style.background = 'dodgerblue';
   }
 }
 
-let i = 0
-function slider() {
-  if (i === data.length) {
-    i = 0
-    addData(i)
-  } else {
-    addData(i)
-    i += 1
-  }
-}
-
-setInterval(slider, 2000);
+//
+// let i = 0
+// function slider() {
+//   if (i === data.length) {
+//     i = 0
+//     addData(i)
+//   } else {
+//     addData(i)
+//     i += 1
+//   }
+// }
+//
+// setInterval(slider, 2000);
